1cd7f112b71696e6caf13490784c8df3cc3f01f5
Alex <alex@alexseifert.com>
2019-04-26 08:52:15 +0200

    #18: convert the settings to controller to TypeScript

---
+++ server/src/controllers/api/settings.ts
@@ -0,0 +1,37 @@
+import { Router, Request, Response } from 'express';
+
+import { returnError } from '../../lib/apiErrorHandling';
+
+import Controller from '../../interfaces/Controller';
+
+
+class SettingsController implements Controller {
+    public router: Router;
+
+    public constructor(router: Router) {
+        this.router = router;
+        this.initilizeRoutes();
+    }
+
+    private initilizeRoutes(): void {
+        this.router.get('/', this.getIndex);
+        this.router.put('/', this.putIndex);
+    }
+
+    private getIndex(req: Request, res: Response): void {
+        res.json({});
+    }
+
+    private putIndex(req: Request, res: Response): void {
+        try {
+            res.status(201).send('');
+        }
+        catch(error) {
+            returnError(error, req, res);
+        }
+    }
+}
+
+export default (router: Router): void => {
+    new SettingsController(router);
+};
