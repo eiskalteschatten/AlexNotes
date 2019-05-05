b6396c9ecfe9d0b144a58921c15b2ec58f588c54
Alex <alex@alexseifert.com>
2019-05-05 15:49:51 +0200

    #19: add Lerna

---
+++ packages/server/config/local.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    defaultUser: {
+        firstName: 'Alex',
+        lastName: 'Seifert',
+        username: 'alexseifert',
+        password: 'alexseifert',
+        emailAddress: 'alex@alexseifert.com'
+    }
+};
