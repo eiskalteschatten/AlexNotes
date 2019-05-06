92c2de7cf012a82069c28ecf822b3cc48cf6aded
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-05-06 18:32:59 +0200

    #7: check git auth type and add protocol to url if missing

--- packages/server/src/lib/git.ts
+++ packages/server/src/lib/git.ts
@@ -7,6 +7,20 @@ const gitConfig: GitConfigInterface = config.get('git');
 
 
 class Git {
+    private url: string = gitConfig.url;
+
+    public constructor() {
+        if (gitConfig.auth.type === 'ssh' && this.url.substring(0,6) !== 'ssh://') {
+            this.url = 'ssh://' + this.url;
+        }
+        else if (gitConfig.auth.type === 'https' && this.url.substring(0,7) !== 'https://') {
+            this.url = 'https://' + this.url;
+        }
+        else if (gitConfig.auth.type !== 'ssh' && gitConfig.auth.type !== 'https') {
+            throw new Error('The git auth type must be "ssh" or "https".');
+        }
+    }
+
     public async initialize(): Promise<void> {
         try {
             await this.clone();
@@ -22,7 +36,7 @@ class Git {
                 checkoutBranch: gitConfig.branch
             };
 
-            const repository: Repository = await Clone.clone(gitConfig.url, gitConfig.localPath, cloneOptions);
+            const repository: Repository = await Clone.clone(this.url, gitConfig.localPath, cloneOptions);
             console.log(repository);
         }
         catch(error) {
