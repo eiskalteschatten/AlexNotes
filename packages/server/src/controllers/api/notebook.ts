f8ef5987cba032d96c937ce25b644cf311d616ed
Alex <alex@alexseifert.com>
2019-05-07 21:58:27 +0200

    #9: add notebook controller

---
+++ packages/server/src/controllers/api/notebook.ts
@@ -0,0 +1,39 @@
+import { Router, Request, Response } from 'express';
+
+import Controller from '../../interfaces/Controller';
+
+import { NotebookMenuItemInterface } from '../../../../shared/types/notebook';
+
+
+class NotebookController implements Controller {
+    public router: Router;
+
+    public constructor(router: Router) {
+        this.router = router;
+        this.initilizeRoutes();
+    }
+
+    private initilizeRoutes(): void {
+        this.router.get('/', this.getIndex);
+    }
+
+    private getIndex(req: Request, res: Response): void {
+        const notebooks: NotebookMenuItemInterface[] = [
+            { title: 'Notebook 2', icon: 'book', id: 'notebook2' },
+            { title: 'Notebook 1', icon: 'book', id: 'notebook1' },
+            { title: 'Notebook 3', icon: 'book', id: 'notebook3' }
+        ];
+
+        notebooks.sort((a, b): number => {
+            if (a.title < b.title) return -1;
+            if (a.title > b.title) return 1;
+            return 0;
+        });
+
+        res.json(notebooks);
+    }
+}
+
+export default (router: Router): void => {
+    new NotebookController(router);
+};
