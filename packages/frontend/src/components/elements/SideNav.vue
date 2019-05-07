5b1b5b635e03eab44cb7d47e0b02b9814df81760
Alex <alex@alexseifert.com>
2019-05-07 21:58:04 +0200

    #9: fix linting

--- packages/frontend/src/components/elements/SideNav.vue
+++ packages/frontend/src/components/elements/SideNav.vue
@@ -20,9 +20,9 @@
         mobile-break-point="960"
     >
         <v-layout column fill-height>
-             <v-btn class="ma-4">
-                 <v-icon left>add</v-icon>
-                 {{ $t('createNotebook') }}
+            <v-btn class="ma-4">
+                <v-icon left>add</v-icon>
+                {{ $t('createNotebook') }}
             </v-btn>
 
             <v-list>
