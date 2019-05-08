5c1465761ea629e10e1613bb91b98c6ba652f910
Alex <alex@alexseifert.com>
2019-05-08 23:13:19 +0200

    #8: create the new notebook dialog

--- packages/frontend/src/components/elements/SideNav/Notebooks.vue
+++ packages/frontend/src/components/elements/SideNav/Notebooks.vue
@@ -1,18 +1,46 @@
 <i18n>
 {
     "en": {
-        "createNotebook": "Create Notebook"
+        "createNotebook": "Create Notebook",
+        "name": "Name",
+        "cancel": "Cancel",
+        "save": "Save"
     },
     "de": {
-        "createNotebook": "Notizbuch erstellen"
+        "createNotebook": "Notizbuch erstellen",
+        "name": "Name",
+        "cancel": "Abbrechen",
+        "save": "Speichern"
     }
 }
 </i18n>
 
 <template>
     <div>
+        <v-dialog v-model="newNotebookDialog" max-width="350">
+            <v-card>
+                <v-card-title class="headline">{{ $t('createNotebook') }}</v-card-title>
+
+                <v-card-text>
+                    <v-text-field :label="`${$t('name')}*`" required model="newNotebookName"></v-text-field>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn flat="flat" @click="closeNewNotebookDialog">
+                        {{ $t('cancel') }}
+                    </v-btn>
+
+                    <v-btn color="success" @click="saveNewNotebook" :loading="newNotebookIsSaving">
+                        {{ $t('save') }}
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
         <div class="text-xs-center">
-            <v-btn class="ma-4">
+            <v-btn class="ma-4" @click="newNotebookDialog = true">
                 <v-icon left>add</v-icon>
                 {{ $t('createNotebook') }}
             </v-btn>
@@ -43,6 +71,13 @@
     import { mapState, mapActions, mapMutations } from 'vuex';
 
     export default Vue.extend({
+        data() {
+            return {
+                newNotebookDialog: false,
+                newNotebookName: '',
+                newNotebookIsSaving: false
+            };
+        },
         computed: {
             ...mapState('settings', [
                 'theme'
@@ -68,9 +103,17 @@
             getActiveClass(isActive: boolean): string {
                 return isActive ? this.activeClass : '';
             },
+            saveNewNotebook(): void {
+                this.newNotebookIsSaving = true;
+                this.closeNewNotebookDialog();
+            },
+            closeNewNotebookDialog(): void {
+                this.newNotebookName = '';
+                this.newNotebookDialog = false;
+                this.newNotebookIsSaving = false;
+            },
             selectNotebook(id: string): void {
                 this.setSelectedNotebookId(id);
-                this.$router.push({ name: 'home' });
             }
         }
     });
