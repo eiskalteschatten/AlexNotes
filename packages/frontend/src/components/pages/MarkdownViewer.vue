4c2aa5f507b178b1090c25773c46d9e1512ef5e7
Alex <alex@alexseifert.com>
2019-06-02 11:10:57 +0200

    Add edit note button

--- packages/frontend/src/components/pages/MarkdownViewer.vue
+++ packages/frontend/src/components/pages/MarkdownViewer.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>
-        <span v-html="renderedHtml" />
-    </div>
+    <v-flex fill-height>
+        <v-btn
+            fixed
+            dark
+            fab
+            bottom
+            right
+            color="secondary"
+            :small="$vuetify.breakpoint.smAndDown"
+            @click="$router.push({ name: 'editNote' })"
+        >
+            <v-icon style="height: auto;">edit</v-icon>
+        </v-btn>
+
+        <div v-html="renderedHtml" />
+    </v-flex>
 </template>
 
 <script lang="ts">
