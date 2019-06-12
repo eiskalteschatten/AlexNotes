7ba5ae648e745674d828aa5582cc93b7650f7534
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-06-12 18:08:37 +0200

    Don't test dist directories

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    coveragePathIgnorePatterns: ['/node_modules/', '/config/test.js'],
-    testPathIgnorePatterns: ['/node_modules/', '/config/test.js'],
+    coveragePathIgnorePatterns: ['/node_modules/', '/config/test.js', '/dist/'],
+    testPathIgnorePatterns: ['/node_modules/', '/config/test.js', '/dist/'],
     collectCoverage: true,
     coverageDirectory: './test-coverage'
 };
