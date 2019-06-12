3de992197385b9b7db121dd8e1ae6c291e0f356c
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-06-12 18:07:54 +0200

    Add test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    coveragePathIgnorePatterns: ['/node_modules/', '/**/config/test.js'],
-    testPathIgnorePatterns: ['/node_modules/', '/config/test.js']
+    coveragePathIgnorePatterns: ['/node_modules/', '/config/test.js'],
+    testPathIgnorePatterns: ['/node_modules/', '/config/test.js'],
+    collectCoverage: true,
+    coverageDirectory: './test-coverage'
 };
