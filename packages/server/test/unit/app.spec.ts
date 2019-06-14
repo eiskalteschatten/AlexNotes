49f5017b717705e3b5cfe6a53e432c0ffde2849a
Alex <alex@alexseifert.com>
2019-06-14 20:04:39 +0200

    Setup express static test

--- packages/server/test/unit/app.spec.ts
+++ packages/server/test/unit/app.spec.ts
@@ -1,8 +1,22 @@
-import mainApp from '../../src/app';
+import * as request from 'supertest';
+
+import App from '../../src/app';
 
 describe('The main app', () => {
+    let app;
+
+    beforeAll(async (done) => {
+        app = await App.setupApp();
+        done();
+    });
+
     test('is set up', async () => {
-        const setupApp = await mainApp.setupApp();
-        expect(setupApp).resolves;
+        expect(app).toBeDefined();
+    });
+
+    test('Express static ("/") works', async () => {
+        const response: request.Response = await request(app).get('/');
+        expect(response.text).toMatch(/\<html(.*)\>/);
+        expect(response.status).toEqual(200);
     });
 });
