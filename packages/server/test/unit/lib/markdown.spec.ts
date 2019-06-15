fba0c4304a24400080d8b2613df4ff234f806cfa
Alex <alex@alexseifert.com>
2019-06-15 20:20:51 +0200

    Add test for markdown lib

---
+++ packages/server/test/unit/lib/markdown.spec.ts
@@ -0,0 +1,17 @@
+import * as request from 'supertest';
+
+import { markdownToHtml, markdownToHtmlWithCodeHighlighting } from '../../../src/lib/markdown';
+
+describe('The api/git controller', () => {
+    test('Convert Markdown to HTML', async () => {
+        const markdown = '# Test';
+        const html = markdownToHtml(markdown);
+        expect(html).toEqual("<h1 id=\"test\">Test</h1>\n");
+    });
+
+    test('Convert Markdown to HTML with Syntax Highlighting', async () => {
+        const markdown = '`const variable = "";`';
+        const html = markdownToHtmlWithCodeHighlighting(markdown);
+        expect(html).toEqual("<p><code>const variable = &quot;&quot;;</code></p>\n");
+    });
+});
